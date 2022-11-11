f59e3100af93d6797b59d2957711c9947307fd53
Александр <ushakov.a.v1@yandex.ru>
2022-11-11 14:52:27 +0700

    Add EventSupplire and RastrSupplire

---
+++ MofelForEfficiency/RastrSupplier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using ASTRALib;
+
+namespace MofelForEfficiency
+{
+    /// <summary>
+    /// Класс, осуществляющий взаимодействие с RastrWin3
+    /// </summary>
+    public abstract class RastrSupplier
+    {
+        /// <summary>
+        /// Экземпляр класса Rastr
+        /// </summary>
+        public static Rastr _rastr = new Rastr();
+
+        /// <summary>
+        /// Загрузка файла в рабочую область 
+        /// </summary>
+        public static void LoadFile(string filePath, string shablon)
+        {
+            _rastr.Load(RG_KOD.RG_REPL, filePath, shablon);
+        }
+    }
+}
